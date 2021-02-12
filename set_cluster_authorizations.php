c1a3afa2daee2d7b5523431c2302d48b48e8f1d0
ultrascan3 system user <us3@tg-mcb170057-run-uslims229-000.novalocal>
2021-02-12 17:22:35 +0000

    added: set_cluster_authorizations.php

---
+++ set_cluster_authorizations.php
@@ -0,0 +1,143 @@
+<?php
+
+$self = __FILE__;
+
+$notes = <<<__EOD
+usage: $self db {updatepeople}
+
+reads cluster_config.php and sets uslims3_DB.people.clusterAuthorizations defaults
+if updatepeople specified, all uslims3_DB.people will get their clusters also set to the new defaults
+
+__EOD;
+
+if ( !( count( $argv ) == 2 || ( count( $argv ) == 3 && $argv[2] == "updatepeople" ) ) ) {
+    echo $notes;
+    exit;
+}
+
+$lims_db       = $argv[ 1 ];
+$update_people = count( $argv ) == 3 && $argv[ 2 ] == "updatepeople";
+
+$us3bin = exec( "ls -d ~us3/lims/bin" );
+include "$us3bin/listen-config.php";
+
+# ********* start user defines *************
+
+# logging_level 
+# 0 : minimal messages (expected value for production)
+# 1 : add detailed db messages
+# 2 : add idle polling messages
+$logging_level      = 3;
+
+# www_uslims3 should likely be in listen_config.php
+$www_uslims3         = "/srv/www/htdocs/uslims3";
+$www_uslims3_newlims = "$www_uslims3/uslims3_newlims";
+$cluster_config_name = "cluster_config.php";
+
+# ********* end user defines ***************
+
+# ********* start admin defines *************
+# these should only be changed by developers
+$db                                = "gfac";
+# ********* end admin defines ***************
+
+function write_logl( $msg, $this_level = 0 ) {
+    global $logging_level;
+    global $self;
+    if ( $logging_level >= $this_level ) {
+        echo "${self}: " . $msg . "\n";
+        # write_log( "${self}: " . $msg );
+    }
+}
+
+function error( $msg ) {
+    write_logl( $msg );
+    exit(-1);
+}
+
+function truestr( $val ) {
+    return $val ? "true" : "false";
+}
+
+function db_obj_result( $db_handle, $query ) {
+    $result = mysqli_query( $db_handle, $query );
+
+    if ( !$result || !$result->num_rows ) {
+        if ( $result ) {
+            # $result->free_result();
+        }
+        write_logl( "db query failed : $query" );
+        exit;
+    }
+
+    if ( $result->num_rows > 1 ) {
+        write_logl( "WARNING: db query returned " . $result->num_rows . " rows : $query" );
+    }    
+
+    return mysqli_fetch_object( $result );
+}
+
+function db_result( $db_handle, $query ) {
+    $result = mysqli_query( $db_handle, $query );
+    if ( !$result ) {
+        write_logl( "db query failed : $query" );
+        exit;
+    }
+    return $result;
+}
+
+do {
+    $db_handle = mysqli_connect( $dbhost, $user, $passwd, $db );
+    if ( !$db_handle ) {
+        write_logl( "could not connect to mysql: $dbhost, $user, $db. Will retry in ${poll_sleep_seconds}s" );
+        sleep( $poll_sleep_seconds );
+    }
+} while ( !$db_handle );
+
+# does database exist
+
+$db_exists = db_obj_result( $db_handle, 
+                            "SELECT schema_name from information_schema.schemata WHERE schema_name='$lims_db'" );
+# write_logl( "Notice: database $lims_db exists" );
+
+$cluster_config = "$www_uslims3/$lims_db/$cluster_config_name";
+
+if ( !file_exists( $cluster_config ) ) {
+    # write_logl( "Notice: $cluster_config does not exist" );
+    $cluster_config = "$www_uslims3_newlims/$cluster_config_name";
+}
+if ( !file_exists( $cluster_config ) ) {
+    error( "Error: $cluster_config does not exist" );
+}
+    
+write_logl( "Notice: reading $cluster_config" );
+
+include "$cluster_config";
+
+if ( !isset( $cluster_configuration ) ) {
+    error( "Error: $cluster_config did not define \$cluster_configuration" );
+}
+
+# build up option string
+
+$authclusts = [];
+
+foreach ( $cluster_configuration as $k => $v ) {
+    if ( isset( $v["active"] ) && $v["active"] ) {
+        $authclusts[] = $k;
+    }
+}
+
+$defaultclustauth = implode( ":", $authclusts );
+
+write_logl( "default clusterAuthorizations: '$defaultclustauth'" );
+
+# alter table
+$result = db_result( $db_handle,
+                     "alter table ${lims_db}.people alter clusterAuthorizations set default '$defaultclustauth'" );
+write_logl( "${lims_db}.people default clusterAuthorizations updated" );
+if ( $update_people ) {
+    $result = db_result( $db_handle,
+                         "update ${lims_db}.people set clusterAuthorizations='$defaultclustauth'" );
+    write_logl( "${lims_db}.people all people clusterAuthorizations updated" );
+}
