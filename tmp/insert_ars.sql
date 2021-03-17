661d3bff30914482a56a81083c66010c0cff58e8
ehb54 <brookes@uthscsa.edu>
2021-03-17 19:08:22 +0000

    remove old sql files, rename tmp autoflow_util, add  to listen-config.template

--- tmp/insert_ars.sql
+++
@@ -1,9 +0,0 @@
-delete from autoflowAnalysis;
-delete from autoflowAnalysisHistory;
-insert into autoflowAnalysis
-    (tripleName, filename, aprofileGUID, invID, statusJson)
-    values
-    ( "triple1", "file1", "guid1", 1, '{"to_process":["2DSA","2DSA_FM","FITMEN","2DSA_IT", "2DSA_MC"]}'),
-    ( "triple2", "file2", "guid2", 2, '{"to_process":[{"2DSA": { "option_2DSA_1" : 123 }}, "2DSA_FM" ]}' ),
-    ( "triple3", "file3", "guid3", 3, '{"to_process":[{"GA":{ "option_GA_3" : 123 }}]}' )
-    ;
